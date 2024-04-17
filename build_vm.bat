12e85473039e17afba36749ca4d5a304232ef400
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-04-17 15:37:48 +0200

    clean up

--- build_vm.bat
+++ build_vm.bat
@@ -1,3 +1,4 @@
+mkdir output
 vagrant scp build_vm.sh default:/home/vagrant/
 vagrant ssh -c "/home/vagrant/build_vm.sh"
-vagrant scp default:/home/vagrant/spiderbot/target/armv7-unknown-linux-gnueabihf/debug/spiderbot spiderbot
+vagrant scp default:/home/vagrant/spiderbot/target/armv7-unknown-linux-gnueabihf/debug/spiderbot output/spiderbot
