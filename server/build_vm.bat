c14bdb844cd18d20e2adaa2b2db883b87581a0ed
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-04-28 10:53:57 +0200

    update

--- server/build_vm.bat
+++ server/build_vm.bat
@@ -1,5 +1,6 @@
 mkdir output
 vagrant ssh -c "mkdir spiderbot"
+vagrant scp update_rust.sh default:/home/vagrant/
 vagrant scp build.sh default:/home/vagrant/
 
 vagrant scp .cargo default:/home/vagrant/spiderbot
@@ -7,5 +8,6 @@ vagrant scp src default:/home/vagrant/spiderbot
 vagrant scp Cargo.lock default:/home/vagrant/spiderbot
 vagrant scp Cargo.toml default:/home/vagrant/spiderbot
 
+vagrant ssh -c "/home/vagrant/update_rust.sh"
 vagrant ssh -c "/home/vagrant/build.sh"
 vagrant scp default:/home/vagrant/spiderbot/target/armv7-unknown-linux-gnueabihf/debug/spiderbot output/spiderbot
--- server/cross_build.sh
+++ server/cross_build.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 cross build --target armv7-unknown-linux-gnueabihf
